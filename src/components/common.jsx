19b313cdac79a76c6e1b8c6cc57ca9cc0109ec82
Wildan <wildanwibisono52@gmail.com>
2026-06-19 00:04:10 +0700

    Ganti logo

--- src/components/common.jsx
+++ src/components/common.jsx
@@ -1,12 +1,11 @@
 import { useReveal, useCountUp } from "../hooks";
 
 // Heart logo mark
-export function BrandMark({ size = 22 }) {
+export function BrandMark({ size = 32 }) {
+  const containerSize = size + 12;
   return (
-    <span className="brand-mark">
-      <svg viewBox="0 0 24 24" width={size} height={size} fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-        <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
-      </svg>
+    <span className="brand-mark" style={{ width: containerSize, height: containerSize }}>
+      <img src="/POLINEMA_CARE.png" alt="Logo" style={{ width: size, height: size, objectFit: 'contain' }} />
     </span>
   );
 }
